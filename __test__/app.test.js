03cb66d951c30abf9e5275e886c31e524bbf8767
naveenkumarbhat <naveenbhat1414@gmail.com>
2024-04-17 14:47:47 +0530

    added test file

---
+++ __test__/app.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const server = require('../index'); // Import the server instance
+
+test('GET / responds with "Hello, World!"', async () => {
+  const response = await request(server).get('/'); // Use server instance instead of app
+  expect(response.status).toBe(200);
+  expect(response.text).toBe('Hello, World!');
+});
